671b9738811e696f19e231c6e4feb984c3fa8e93
JuliWolf <qwert0038@yandex.ru>
2022-02-12 11:57:04 +0300

    Add files from lesson 7 and lesson 8

---
+++ src/main/java/Lesson_7/lesson/Graph.java
@@ -0,0 +1,24 @@
+package Lesson_7.lesson;
+
+public interface Graph {
+
+    void addVertex(String label);
+
+    boolean addEdge(String startLabel, String secondLabel, String... others);
+    boolean addEdge(String startLabel, String secondLabel);
+
+    int getSize();
+
+    void display();
+
+    /**
+     * англ. Depth-first search, DFS
+     */
+    void dfs(String startLabel);
+
+    /**
+     * англ. breadth-first search, BFS
+     */
+    void bfs(String startLabel);
+
+}
